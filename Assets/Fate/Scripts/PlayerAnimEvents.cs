b1fbe5853ae688b37b10c837d05fc70369c5b292
Fate-Ku <elaina42fate@gmail.com>
2024-11-10 23:11:50 +0900

    2024/11/10 update

---
+++ Assets/Fate/Scripts/PlayerAnimEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimEvents : MonoBehaviour
+{
+    private Player_F player;
+
+    void Start()
+    {
+        player = GetComponentInParent<Player_F>();
+    }
+
+    private void AnimationTrigger()
+    {
+        player.AttackOver();
+    }
+
+}
--- Assets/Fate/Scripts/Player_F.cs
+++ Assets/Fate/Scripts/Player_F.cs
@@ -10,6 +10,20 @@ public class Player_F : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
 
+    [Header("Dash info")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    private float dashTime;
+
+    [SerializeField] private float dashCooldown;
+    private float dashCooldownTimer;
+
+    [Header("Attack info")]
+    [SerializeField] private float comboTime = 0.3f;
+    private float comboTimeWindow;
+    private bool isAttacking;
+    private int comboCounter;
+
     private float xInput; // key input
 
     private int facingDir = 1; // facing Direction
@@ -17,7 +31,7 @@ public class Player_F : MonoBehaviour
 
     [Header("Collision info")]
     [SerializeField] private float groundCheckDistance; // check ground disance
-    [SerializeField] private LayerMask whatIsGround; 
+    [SerializeField] private LayerMask whatIsGround;
     private bool isGrounded; // is grounded or not
 
     void Start()
@@ -32,14 +46,42 @@ public class Player_F : MonoBehaviour
         CheckInput();
         CollisionChecks();
 
+        dashTime -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;
+        comboTimeWindow -= Time.deltaTime;
+
         FlipController();
         AnimatorControllers();
+    }
+
+    public void AttackOver()
+    {
+        isAttacking = false;
+
+        comboCounter++;
+
+        if (comboCounter > 2)
+        {
+            comboCounter = 0;
+        }
+
     }
 
     // move
     private void Movement()
     {
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        if (isAttacking)
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+        else if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(facingDir * dashSpeed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        }
     }
 
     // check if input space or not
@@ -47,9 +89,48 @@ public class Player_F : MonoBehaviour
     {
         xInput = Input.GetAxisRaw("Horizontal");
 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            StartAttackEvent();
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            DashAbility();
+        }
+
+    }
+
+    // start attack event
+    private void StartAttackEvent()
+    {
+        if (!isGrounded)
+        {
+            return;
+        }
+        if (comboTimeWindow < 0)
+        {
+            comboCounter = 0;
+        }
+
+
+        isAttacking = true;
+        comboTimeWindow = comboTime;
+
+    }
+
+    // dash
+    private void DashAbility()
+    {
+        if(dashCooldownTimer < 0 && !isAttacking)
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTime = dashDuration;
         }
     }
 
@@ -67,10 +148,12 @@ public class Player_F : MonoBehaviour
     {
         bool isMoving = rb.velocity.x != 0;
 
-        anim.SetFloat("yVelocity",rb.velocity.y);
+        anim.SetFloat("yVelocity", rb.velocity.y);
         anim.SetBool("isMoving", isMoving);
         anim.SetBool("isGrounded", isGrounded);
-
+        anim.SetBool("isDashing", dashTime > 0); ;
+        anim.SetBool("isAttacking",isAttacking);
+        anim.SetInteger("comboCounter", comboCounter);
 
     }
 
@@ -79,7 +162,7 @@ public class Player_F : MonoBehaviour
     {
         facingDir = facingDir * -1;
         facingRight = !facingRight;
-        transform.Rotate(0,180,0);
+        transform.Rotate(0, 180, 0);
 
     }
 
