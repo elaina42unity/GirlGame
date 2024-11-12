95cbca2027113a91cee49b215f13180e8b646aae
NeroSaika1024 <elaina42axium@gmail.com>
2024-11-12 17:14:24 +0900

    1112

---
+++ Assets/NeroSaika/Scripts/PlayerMoveStateAX.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveStateAX : PlayerStateAX
+{
+    public PlayerMoveStateAX(PlayerAX _player, PlayerStateMachineAX _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
