dfb250a4028f7552cd7a73b5d276f34d9efce39a
Admintor <elaina42ash@gmail.com>
2024-11-17 14:43:47 +0900

    2024/11/17 14:41
    Ash个人项目更新

---
+++ Assets/DepartureAsh/Scripts/Skills/CloneSkillAsh.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneSkillAsh : SkillAsh
+{
+
+    [Header("Clone info")]
+    [SerializeField] private GameObject clonePrefab_;
+    [SerializeField] private float cloneDuration_;
+    [SerializeField] private bool canAttack_;
+
+    public void CreateClone(Transform clonePosition)
+    {
+        GameObject newClone = Instantiate(clonePrefab_);
+        newClone.GetComponent<CloneSkillControllerAsh>().SetupClone(clonePosition, cloneDuration_ , canAttack_);
+    }
+}
