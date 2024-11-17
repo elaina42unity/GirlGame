e9f30027984825d688dfb4c84c3af75e31a87591
Renne-Entropy <elaina42renne@gmail.com>
2024-11-17 18:11:24 +0900

    Renne分支 TileMap/背景

---
+++ Assets/Renne/Scripts/PlayerAnimationTriggersRenne.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationTriggersRenne : MonoBehaviour
+{
+    private PlayerRenne player => GetComponentInParent<PlayerRenne>();
+    private void AnimationTrigger()
+    {
+        player.AnimationTrigger();
+    }
+}
