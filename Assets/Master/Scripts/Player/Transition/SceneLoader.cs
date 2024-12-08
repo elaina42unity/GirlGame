e6f18f2a042bfff77539624b2508105fbd3b67ac
Renne-Entropy <elaina42renne@gmail.com>
2024-12-08 19:23:25 +0900

    场景修改

--- Assets/Master/Scripts/Player/Transition/SceneLoader.cs
+++ Assets/Master/Scripts/Player/Transition/SceneLoader.cs
@@ -3,19 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public Transform playerTrans;
+
     [Header("イベントリスナー")]
     public SceneLoadEventSO loadEventSO;
     public GameSceneSO firstLoadScene;
 
     [SerializeField] private GameSceneSO currentLoadedScene;
-    [SerializeField] private GameSceneSO sceneToLoad;
+    private GameSceneSO sceneToLoad;
     private Vector3 positionToGo;
-    private bool fadeScreen;
 
+    private bool fadeScreen;
+    private bool isLoading;
     public float fadeDuration;
 
     private void Awake()
@@ -27,15 +32,22 @@ public class SceneLoader : MonoBehaviour
 
     private void OnEnable()
     {
+        //イベントを登録
         loadEventSO.LoadRequestEvent += OnLoadRequestEvent;
     }
 
     private void OnDisable()
     {
+        //イベントを取り消し
         loadEventSO.LoadRequestEvent -= OnLoadRequestEvent;
     }
     private void OnLoadRequestEvent(GameSceneSO locationToLoad, Vector3 posToGo, bool fadeScreen)
     {
+        if (isLoading)
+        {
+            return;
+        }
+        isLoading = true;
         sceneToLoad = locationToLoad;
         positionToGo = posToGo;
         this.fadeScreen = fadeScreen;
@@ -63,6 +75,28 @@ public class SceneLoader : MonoBehaviour
 
     private void LoadNewScene()
     {
-        sceneToLoad.sceneReference.LoadSceneAsync(LoadSceneMode.Additive, true);
+        var loadingOption = sceneToLoad.sceneReference.LoadSceneAsync(LoadSceneMode.Additive, true);
+        loadingOption.Completed += OnLoadCompleted;
+    }
+
+    /// <summary>
+    /// シーンのロード完了後
+    /// </summary>
+    /// <param name="handle"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    private void OnLoadCompleted(AsyncOperationHandle<SceneInstance> handle)
+    {
+        //currentシーンを次のシーンに遷移
+        currentLoadedScene = sceneToLoad;
+
+        //シーン遷移した後のplayerの移動
+        playerTrans.position = positionToGo;
+
+        if (fadeScreen)
+        {
+
+        }
+
+        isLoading = false;
     }
 }
